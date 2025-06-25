44fc93b42bf4bd39a94c14afcb638eaf2bb7761a
hicham1210 <Bellaj.hicham1@gmail.com>
2025-06-25 16:41:20 +0200

    commit and push

--- astronaut-bio/first-astronaut-bio/New File.js
+++ astronaut-bio/first-astronaut-bio/New File.js
@@ -1,12 +1,14 @@
 let button = document.getElementById("launchBtn");
 
+// Function to launch the mission with countdown
 function launchMission() {
     for (let i = 0; i < 4; i++) {
        if (i === 3) {alert("LAUNCH");
     }else {
-        alert(` launching in ${i+1}`); }
+        alert(`launching in ${i+1}`); }
     document.getElementById("MissionStatus").textContent = "Not Mission Complete";   
     }
 }
 
+// Add event listener to the button
 button.addEventListener("click",launchMission);
